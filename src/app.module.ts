51ce8266253db47a0bc66122cc9c7f1b98b91eba
Misha Poleshchenkov <mikhail.poleshchenkov@mail.ru>
2022-02-08 03:30:00 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,15 +19,15 @@ import { UsersModule } from "./users/users.module"
       port: 5432,
       username: process.env.POSTGRES_USERNAME || "postgres",
       password: process.env.POSTGRES_PASSWORD || "admin",
-      database: process.env.POSTGRES_DATABASE || "localdb",
+      database: process.env.POSTGRES_DATABASE || "smartbook-test",
       models: [],
       autoLoadModels: true,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      }
+      // dialectOptions: {
+      //   ssl: {
+      //     require: true,
+      //     rejectUnauthorized: false
+      //   }
+      // }
     }),
     UsersModule
   ],
