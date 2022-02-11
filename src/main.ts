5269f59f36399bf3dce581183c3cf357c616a0c9
Misha Poleshchenkov <mikhail.poleshchenkov@mail.ru>
2022-02-11 20:00:09 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin: true
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   const config = new DocumentBuilder()
